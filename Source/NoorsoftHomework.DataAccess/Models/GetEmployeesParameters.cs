17af5cfa6e7dab667d273462f4800a9f871bf0b6
Seyed Hossein Hosseini <payoff900@gmail.com>
2021-05-04 01:23:14 +0430

    Modify the get employee parameters to use an enum for the column name property.

--- Source/NoorsoftHomework.DataAccess/Models/GetEmployeesParameters.cs
+++ Source/NoorsoftHomework.DataAccess/Models/GetEmployeesParameters.cs
@@ -1,13 +1,37 @@
 namespace NoorsoftHomework.DataAccess.Models
 {
+    public record GetEmployeesParameters
+    {
+        public string ColumnNameToOrderBy { get; }
+        public string DirectionToOrderBy  { get; }
+        public int    Offset              { get; }
+        public int    Count               { get; }
+
+        public GetEmployeesParameters(SortColumn    columnNameToOrderBy,
+                                      SortDirection directionToOrderBy,
+                                      int           offset,
+                                      int           count)
+        {
+            DirectionToOrderBy  = directionToOrderBy.ToString();
+            ColumnNameToOrderBy = columnNameToOrderBy.ToString();
+            Offset              = offset;
+            Count               = count;
+        }
+    }
+
+    public enum SortColumn
+    {
+        Id,
+        FirstName,
+        LastName,
+        BirthDate,
+        RecruitmentDate,
+        SupervisorId,
+    }
+
     public enum SortDirection
     {
         ASC,
         DESC
     }
-
-    public record GetEmployeesParameters(string        ColumnNameToOrderBy,
-                                        SortDirection DirectionToOrderBy,
-                                        int           Offset,
-                                        int           Count);
 }
