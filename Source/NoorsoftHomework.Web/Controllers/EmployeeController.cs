fd77307ed2282d5662a0546524e5edf11bb19d1e
Seyed Hossein Hosseini <payoff900@gmail.com>
2021-05-04 21:40:24 +0430

    Create an action and its handler for updating an employee.

--- Source/NoorsoftHomework.Web/Controllers/EmployeeController.cs
+++ Source/NoorsoftHomework.Web/Controllers/EmployeeController.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using NoorsoftHomework.Web.Handlers.Employee.Commands;
 using NoorsoftHomework.Web.Handlers.Employee.Queries;
 using NoorsoftHomework.Web.Resources;
 
@@ -20,7 +21,14 @@ namespace NoorsoftHomework.Web.Controllers
         [HttpGet]
         public async Task<ActionResult<ApiResponse>> Get([FromQuery] SortingAndPagingOption sortingAndPagingOption)
         {
-            var (statusCode,data) = await _sender.Send(sortingAndPagingOption);
+            var (statusCode, data) = await _sender.Send(sortingAndPagingOption);
+            return StatusCode(statusCode, data);
+        }
+
+        [HttpPost("{id}")]
+        public async Task<ActionResult<ApiResponse>> Update(int id, UpdateEmployeeResource resource)
+        {
+            var (statusCode, data) = await _sender.Send(new UpdateEmployeeRequest(id, resource));
             return StatusCode(statusCode, data);
         }
     }
---
+++ Source/NoorsoftHomework.Web/Handlers/Employee/Commands/UpdateEmployeeCommand.cs
@@ -0,0 +1,33 @@
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using NoorsoftHomework.DataAccess.Interfaces;
+using NoorsoftHomework.DataAccess.Models;
+using NoorsoftHomework.Web.Resources;
+
+namespace NoorsoftHomework.Web.Handlers.Employee.Commands
+{
+    public record UpdateEmployeeRequest(int Id, UpdateEmployeeResource Resource) : IRequest<ApiResponse>;
+
+    public class UpdateEmployeeCommand : IRequestHandler<UpdateEmployeeRequest, ApiResponse>
+    {
+        private readonly IMapper             _mapper;
+        private readonly IEmployeeRepository _repository;
+
+        public UpdateEmployeeCommand(IMapper mapper, IEmployeeRepository repository)
+        {
+            _mapper     = mapper;
+            _repository = repository;
+        }
+
+        public async Task<ApiResponse> Handle(UpdateEmployeeRequest request, CancellationToken cancellationToken)
+        {
+            var updateModel = _mapper.Map<UpdateEmployeeModel>(request);
+            await _repository.Update(updateModel);
+            var updatedEmployeeResource = _mapper.Map<EmployeeResource>(updateModel);
+            return new ApiResponse(StatusCodes.Status201Created, updatedEmployeeResource);
+        }
+    }
+}
