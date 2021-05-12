08dddf3dcfb812cf9612ed00f9e161e19f5a9993
Seyed Hossein Hosseini <payoff900@gmail.com>
2021-05-12 18:15:59 +0430

    Modify the api endpoints.

--- Source/NoorsoftHomework.UI/src/utilities/endpoints.js
+++ Source/NoorsoftHomework.UI/src/utilities/endpoints.js
@@ -1,7 +1,9 @@
 const apiRoot = "https://localhost:5001"
 const api = {
-    getEmployees: `${apiRoot}/employee`,
-    deleteEmployee: (id) => `${apiRoot}/employee/${id}`
+    getEmployees: () => `${apiRoot}/employee`,
+    deleteEmployee: (id) => `${apiRoot}/employee/${id}`,
+    updateEmployee: (id) => `${apiRoot}/employee/${id}`,
+    createEmployee: () => `${apiRoot}/employee`,
 }
 export default {
     api
